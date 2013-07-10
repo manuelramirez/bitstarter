d4143faff806a0d5330911f063afc035e4e674e6
Manuel Ramirez <manuel@ramirez-fernandez.net>
2013-07-10 18:10:03 +0200

    Edit web.js require fs

--- web.js
+++ web.js
@@ -2,11 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf=require('buffer');
-
-var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8'); 
-
 app.get('/', function(request, response) {
+    var fs = require('fs');
+    var buf = new Buffer(fs.readFileSync("index.html"), "utf-8"); 
     response.send(buf.toString());
 });
 
