40d3532f0c7c2587f2e29532123740f75185f996
Manuel Ramirez <manuel@ramirez-fernandez.net>
2013-07-10 18:05:49 +0200

    Edit web.js buf

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf=require('buffer');
+
 var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8'); 
 
 app.get('/', function(request, response) {
